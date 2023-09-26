90702b37cc8ab17ab704875ba30459a10a421998
Chimdia Anyiam <fortunechimdiya@gmail.com>
2023-09-26 19:59:07 +0100

    for render

--- src/utils/data-source.ts
+++ src/utils/data-source.ts
@@ -2,23 +2,24 @@ require('dotenv').config({ path: require('find-config')('.env ') });
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import config from 'config';
+import { User } from '../entities/user.entity';
+import { Course } from '../entities/course.entity';
+import BaseModel from '../entities/model.entity';
 
-
-const postgresConfig =
-        config.get<{
-              host: string;
-              port: number;
-              username: string;
-              password: string;
-              database: string;
-          }>('postgresConfig')
+const postgresConfig = config.get<{
+    host: string;
+    port: number;
+    username: string;
+    password: string;
+    database: string;
+}>('postgresConfig');
 const AppDataSource = new DataSource({
     ...postgresConfig,
     type: 'postgres',
     synchronize: true,
     logging: false,
     // ssl: true,
-    entities: ['src/entities/*.entity{.ts,.js}'],
+    entities: [User, Course, BaseModel],
     migrations: ['src/migrations/**/*{.ts,.js}'],
     subscribers: ['src/subscribers/**/*{.ts,.js}'],
 });
